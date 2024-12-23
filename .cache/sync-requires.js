3c996871e5ef62922056e559a34a0d80a90f2e23
joneshshrestha <contact@joneshshrestha.com>
2024-12-23 12:46:15 -0600

    review

---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/joneshshrestha/Desktop/myWebsite/.cache/dev-404-page.js"))
+}
+
